c7a5630253819a2a1df191ba4b4f9a6e56f96e64
hardik4sharma <sharma1807hardik@gmail.com>
2026-09-08 17:09:09 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "hardik4sharma-tfstate-practice"
+    key            = "practice/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
